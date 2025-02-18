b51184642d8763ebdedb7e23b12ec51bd38c723e
Ashish Bagdane <ashishbagdaneofficial@gmail.com>
2025-02-18 15:39:07 +0530

    fix: remove usage of auditing annotation

--- src/main/java/dev/ash/techradar/TechRadarApplication.java
+++ src/main/java/dev/ash/techradar/TechRadarApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @OpenAPIDefinition(info = @Info(
@@ -18,7 +17,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 ))
 @EntityScan(basePackages = {"dev.ash.techradar"})
 @EnableJpaRepositories(basePackages = {"dev.ash.techradar"})
-@EnableJpaAuditing
 @EnableCaching
 @SpringBootApplication
 @Import(ObservabilityConfig.class)
